7afe9fd00881b98940f8ac7a04898ebe4961af23
Kritsana-PC <Kritsana-PC@Kritsana>
2015-09-29 14:02:03 +0700

    new web application

---
+++ src/java/service/Hello.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+/**
+ *
+ * @author Kritsana-PC
+ */
+public class Hello {
+    public String getHelloName(String name){
+        return "Hello "+name;
+    }
+}
